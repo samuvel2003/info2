1cb62c987d28f34365375cce7cf66d065cafb378
samuvel2003 <samuelra2003@gmail.com>
2023-03-24 11:47:18 -0500

    hi

---
+++ Imagenes/opencv.py
@@ -0,0 +1,45 @@
+# Llamar la libreria
+import cv2
+
+#Leer la imagen
+#path = r"C:\Users\Juan Jose\OneDrive\Escritorio\info 2\a.jpg"
+path = r"C:\Users\Juan Jose\OneDrive\Escritorio\info 2\info2-1\a.jpg"
+img=cv2.imread(path)
+
+#Caracteristicas
+
+print (img.shape)
+print (img.size)
+print (img.dtype)
+
+#Tomar los 3 canales
+imgR=img[:,:,0]
+imgG=img[:,:,1]
+imgB=img[:,:,2]
+
+#Entar a los pixeles de la imagen
+print(img[100,100])
+
+#Crear imagen
+cv2.imwrite('ImagenX.png',imgR)
+
+#Crear ventana
+cv2.namedWindow('Ventana',0)
+cv2.namedWindow('VentanaB',0)
+cv2.namedWindow('VentanaG',0)
+cv2.namedWindow('VentanaR',0)
+
+#Mostrar imagen
+cv2.imshow('Ventana',img)
+cv2.imshow('VentanaR',imgB)
+cv2.imshow('VentanaG',imgG)
+cv2.imshow('VentanaB',imgR)
+
+#Detener la imagen
+cv2.waitKey(0)
+#Cerrar las ventanas
+cv2.destroyAllWindows()
+
+
+
+
