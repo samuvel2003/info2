ff44382946d1086b3c38f4252154c62cea458093
samuvel2003 <samuelra2003@gmail.com>
2023-03-31 18:02:10 -0500

    1

--- Imagenes/opencv.py
+++ Imagenes/opencv.py
@@ -43,3 +43,4 @@ cv2.destroyAllWindows()
 
 
 
+
---
+++ Imagenes/parcial_2.py
@@ -0,0 +1,8 @@
+import pymongo as py
+import numpy as np 
+import matplotlib.pyplot as plt 
+import pandas as pd 
+# para trabajar con señales
+import scipy.io as sio
+import cv2
+import seaborn as sns
